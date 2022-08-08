b49d0a12b2d267f4992dcc501f028fc32e4225bb
Tailan Mendes <tm250324@ncr.com>
2022-08-08 14:44:49 -0400

    Add e customização do grafico de temperatura

--- Dashboard.Designer.cs
+++ Dashboard.Designer.cs
@@ -29,9 +29,10 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.chartTemperature = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.chartTemperature)).BeginInit();
@@ -44,25 +45,67 @@
             // 
             // chartTemperature
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chartTemperature.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chartTemperature.Legends.Add(legend2);
-            this.chartTemperature.Location = new System.Drawing.Point(89, 73);
+            this.chartTemperature.BackColor = System.Drawing.Color.Gray;
+            this.chartTemperature.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.TopBottom;
+            chartArea1.AxisX.Interval = 1D;
+            chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisX.IsLabelAutoFit = false;
+            chartArea1.AxisX.IsMarginVisible = false;
+            chartArea1.AxisX.LabelStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            chartArea1.AxisX.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisX.LineWidth = 0;
+            chartArea1.AxisX.MajorGrid.Enabled = false;
+            chartArea1.AxisX.MajorGrid.Interval = 0D;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisX.MajorTickMark.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(75)))), ((int)(((byte)(111)))));
+            chartArea1.AxisX.MajorTickMark.Size = 5F;
+            chartArea1.AxisX.Maximum = 23D;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisX.Title = "Horas";
+            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            chartArea1.AxisY.Interval = 1D;
+            chartArea1.AxisY.IsLabelAutoFit = false;
+            chartArea1.AxisY.LabelStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            chartArea1.AxisY.LabelStyle.Format = "0*C";
+            chartArea1.AxisY.LineColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisY.Maximum = 35D;
+            chartArea1.AxisY.Minimum = 28D;
+            chartArea1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(45)))), ((int)(((byte)(86)))));
+            chartArea1.BorderWidth = 5;
+            chartArea1.Name = "ChartArea1";
+            this.chartTemperature.ChartAreas.Add(chartArea1);
+            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            legend1.IsTextAutoFit = false;
+            legend1.Name = "Legend1";
+            this.chartTemperature.Legends.Add(legend1);
+            this.chartTemperature.Location = new System.Drawing.Point(37, 30);
             this.chartTemperature.Name = "chartTemperature";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chartTemperature.Series.Add(series2);
-            this.chartTemperature.Size = new System.Drawing.Size(617, 300);
+            series1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.TopBottom;
+            series1.BorderWidth = 5;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.SplineArea;
+            series1.Color = System.Drawing.Color.Firebrick;
+            series1.Legend = "Legend1";
+            series1.LegendText = "Temperatura em °C";
+            series1.Name = "Temp";
+            series1.ShadowColor = System.Drawing.Color.Transparent;
+            this.chartTemperature.Series.Add(series1);
+            this.chartTemperature.Size = new System.Drawing.Size(1099, 499);
             this.chartTemperature.TabIndex = 0;
             this.chartTemperature.Text = "Temperature";
+            title1.Alignment = System.Drawing.ContentAlignment.TopLeft;
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            title1.Name = "Title1";
+            title1.Text = "Temperatura";
+            this.chartTemperature.Titles.Add(title1);
+            this.chartTemperature.Click += new System.EventHandler(this.chartTemperature_Click);
             // 
             // frmDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1188, 576);
             this.Controls.Add(this.chartTemperature);
             this.Name = "frmDashboard";
             this.ShowIcon = false;
--- Dashboard.cs
+++ Dashboard.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.IO;
 
 namespace Dashboard_Monitor
 {
@@ -23,6 +25,8 @@ namespace Dashboard_Monitor
         {
             this.bcInt = new DataInterface();
 
+            updateTempChart();
+
             InitializeTimer();
         }
 
@@ -38,10 +42,78 @@ namespace Dashboard_Monitor
 
         private void DownloadData(object sender, EventArgs e)
         {
-            this.bcInt.getContractData();
+            //this.bcInt.getContractData();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
+        {
+            updateTempChart();
+        }
+
+        private void updateTempChart()
+        {
+            this.chartTemperature.Series[0].Points.Clear();
+            //Series series = this.chartTemperature.Series.Add("Temperature");
+            //Series series = this.chartTemperature.Series[0];
+            //series.ChartType = SeriesChartType.Area;
+
+            string data_path = @"c:\\iaq_data.ms";
+
+            int serie_count = chartTemperature.Series.Count();
+            int count_measures = 0;
+
+            using (StreamReader reader = new StreamReader(data_path))
+            {
+                int current_hour = -1;
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    double temperature = Convert.ToDouble(getTempFromString(line));
+
+                    DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(Convert.ToUInt32(getEpochTime(line)));
+
+                    
+
+                    if (dateTimeOffset.Day == 7 & dateTimeOffset.Hour != current_hour)
+                    {
+                        current_hour = dateTimeOffset.Hour;
+                        //chartTemperature.Series[0].Points.AddXY(dateTimeOffset.Hour, temperature);
+                        chartTemperature.Series[0].Points.AddXY(dateTimeOffset.Hour, temperature);
+                        count_measures++;
+                        
+
+                        //series.Points.AddXY(dateTimeOffset.Hour, temperature);
+                    }
+
+                    //series.Points.AddXY(dateTimeOffset.DateTime, temperature);
+                }
+            }
+
+            /*
+            series.Points.AddXY("7", 29);
+            series.Points.AddXY("8", 30);
+            series.Points.AddXY("9", 31);
+            series.Points.AddXY("10", 28);
+            series.Points.AddXY("11", 32);
+            series.Points.AddXY("12", 33);
+            */
+        }
+
+        private string getEpochTime(string s)
+        {
+            string[] subs = s.Split('|');
+            var epochtime = subs[2];
+            return epochtime;
+        }
+
+        private string getTempFromString(String s)
+        {
+            string[] subs = s.Split('|');
+            var sTemp = subs[0];
+            return sTemp;
+        }
+
+        private void chartTemperature_Click(object sender, EventArgs e)
         {
 
         }
